668e68e87b1d523f39904015005f5cd11ba886ba
AishwaryaShelke1911 <147431309+AishwaryaShelke1911@users.noreply.github.com>
2023-10-10 23:36:57 +0530

    Add files via upload
    assignment 1 of my captain : area of circle

---
+++ mycaptainass1.py
@@ -0,0 +1,92 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 1,
+   "id": "86fa3f63-4930-447b-9cd8-f49b014c010e",
+   "metadata": {
+    "tags": []
+   },
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "Hello World\n"
+     ]
+    }
+   ],
+   "source": [
+    "print(\"Hello World\")"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 5,
+   "id": "45aea47e-bd7c-475e-b83f-56c6d0b35a70",
+   "metadata": {
+    "tags": []
+   },
+   "outputs": [
+    {
+     "name": "stdin",
+     "output_type": "stream",
+     "text": [
+      "Enter the radius of the circle: 1.1\n"
+     ]
+    },
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "Area of the circle: 3.8013271108436504\n"
+     ]
+    }
+   ],
+   "source": [
+    "#write the Python program which accepts the radius of the circle from user and compute the area\n",
+    "from math import pi\n",
+    "r=float(input(\"Enter the radius of the circle:\"))\n",
+    "b=pi*r**2\n",
+    "print(\"Area of the circle:\",b)\n"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "cc7fa125-e2a9-4347-a8e3-a2d2339a653b",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "77384de1-8782-44be-b34e-96b161d7aa54",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3 (ipykernel)",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.11.5"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
