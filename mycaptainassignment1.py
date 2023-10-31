5b069629df9c53ecfebb95cb80601ee95d5d816c
AishwaryaShelke1911 <147431309+AishwaryaShelke1911@users.noreply.github.com>
2023-11-01 00:45:58 +0530

    Add files via upload

---
+++ mycaptainassignment1.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 30 19:41:59 2023
+
+@author: Aishwarya
+"""
+
+radius=float(input("enter the radius of the circle:"))
+area=3.14159265359*(radius*radius)
+print(area)
